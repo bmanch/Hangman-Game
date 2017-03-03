d613088d76694c06d9926ea2689b6f6d0dfc5177
Brian Manchester <brian.l.manchester@gmail.com>
2017-03-03 11:21:24 -0500

    the commas are gone

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var wordBank = {
-	w1: ["C", "A", "G", "E"],
-	w2: ["G", "I", "G", "L", "E"]
+	w1: ["G", "I", "G", "L", "E"],
+	w2: ["W", "A", "T", "E", "R", "W", "O", "R", "L", "D"]
 }
 
 var wordBankIndex = 0;
@@ -13,9 +13,15 @@ var blankWord = [];
 
 var theWord = document.getElementById("theWord");
 
+var guesses = 0;
+
 function activeWord () {
 	if (wordBankIndex <= (wordBankArray.length - 1)) {
 		currentWord = wordBankArray[wordBankIndex];
+		if (currentWord = wordBank.w1) {
+			blankWord = [["_ "], ["_ "], ["_ "], ["_ "], ["_ "]];
+			theWord.textContent = blankWord[0] + blankWord[1] + blankWord[2] + blankWord[3] + blankWord[4];
+		}
 	}
 
 	else {
@@ -27,30 +33,28 @@ console.log(currentWord);
 console.log(blankWord);
 
 if (currentWord === wordBank.w1) {
-	blankWord = ["_ ", "_ ", "_ ", "_"];
-	theWord.textContent = blankWord;
-
 	document.onkeyup = function(event) {
 		var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
 
 		if (userGuess === currentWord[0]) {
-			blankWord[0] = event.key;
-			theWord.textContent = blankWord;
+			blankWord[0] = event.key.toUpperCase();
+			blankWord[2] = event.key.toUpperCase();
+			theWord.textContent = blankWord[0] + blankWord[1] + blankWord[2] + blankWord[3] + blankWord[4];
 		}
 
 		else if (userGuess === currentWord[1]) {
-			blankWord[1] = event.key;
-			theWord.textContent = blankWord;
-		}
-
-		else if (userGuess === currentWord[2]) {
-			blankWord[2] = event.key;
-			theWord.textContent = blankWord;
+			blankWord[1] = event.key.toUpperCase();
+			theWord.textContent = blankWord[0] + blankWord[1] + blankWord[2] + blankWord[3] + blankWord[4];
 		}
 
 		else if (userGuess === currentWord[3]) {
-			blankWord[3] = event.key;
-			theWord.textContent = blankWord;
+			blankWord[3] = event.key.toUpperCase();
+			theWord.textContent = blankWord[0] + blankWord[1] + blankWord[2] + blankWord[3] + blankWord[4];
+		}
+
+		else if (userGuess === currentWord[4]) {
+			blankWord[4] = event.key.toUpperCase();
+			theWord.textContent = blankWord[0] + blankWord[1] + blankWord[2] + blankWord[3] + blankWord[4];
 		}
 	}
 
